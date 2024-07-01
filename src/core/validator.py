353255c461a305472a432f31e87c01c7e5e36c43
Niveditha <nivinath7@gmail.com>
2024-07-01 13:35:16 +0530

    Accuracy boost up to about 60% after prompt engineering

--- src/core/validator.py
+++ src/core/validator.py
@@ -1,7 +1,12 @@
 import base64
 import requests
-from src.core import video_understanding
+from src.core import video_understander
 from PIL import Image, ImageDraw
+from colorama import init, Fore, Style
+import os
+from src.core import parser
+
+
 
 class Color:
     RED = '\033[91m'
@@ -15,32 +20,28 @@ class Color:
 
 def validator(params, prompt_GPT, last_frame):  
         
-        # for row in last_frame:
-        #     for i in range(len(row)):
-        #         # Swap Green and Blue channels
-        #         row[i] = (row[i][2], row[i][1], row[i][0])
+        """ Function to call the validator LVLM
 
-
+        Returns:
+            Output of validator LVLM
+        """
+        
 
         # OpenAI API Key
         api_key = params['openai_api_key']
 
         image = Image.fromarray(last_frame)
 
-        image.save("/home/nivi_nath/tmp_reason_ws/src/driver_codes/fm_temporal_reasoning/output/dataset_1/test/test.png")
+        image.save("/home/nivi_nath/tmp_reason_ws/src/driver_codes/fm_temporal_reasoning/output/test.png")
 
-        #get the current frame from robot instead of path or last frame from the video !?
-        # generated_image_path = "/home/nivi_nath/tmp_reason_ws/src/driver_codes/fm_temporal_reasoning/output/dataset_1/test/last_frame4.jpg"
 
-        generated_image_path = "/home/nivi_nath/tmp_reason_ws/src/driver_codes/fm_temporal_reasoning/output/dataset_1/test/test.png"
+        generated_image_path = "/home/nivi_nath/tmp_reason_ws/src/driver_codes/fm_temporal_reasoning/output/test.png"
 
         with open(generated_image_path, "rb") as image_file:
             base64_image = base64.b64encode(image_file.read()).decode('utf-8')
 
-        #saving the last frame as an image
-    
-        
-
+        if os.path.exists(generated_image_path):
+            os.remove(generated_image_path)
 
         headers = {
                     "Content-Type": "application/json",
@@ -82,18 +83,23 @@ def validator(params, prompt_GPT, last_frame):
         else:
             print("Error:", response.status_code)
 
-def conversation(params, modified_prompt, last_frame):
-    # example_output_LVLM = "the object that was dropped was a blue color cloth in the centre of the table next to a white colored bottle"
-    # prompt_GPT = "There is a phrase grounding model that needs to uniquely ground a specific object which will help a robot interact with that object. Given this image, is the context enough of information to uniquely identify an object? Reply with a YES if that object can be distinguished uniquely with the given context. If not, ask more questions as an MCQ to uniquely identify the object using its spatial location or other properties. If object is not present, ask more questions to get the right object. Example - Context: The object that was placed was a bottle. Question: Which one was it? The rightmost, second from right, third from left, right and second from top, right and bottommost, or?"    
-    # prompt_LVLM = "Identify the object that was dropped. The video is from a robot's perspective. Return all answers from the robot's perspective. "
-    prompt_GPT = "There is a phrase grounding model that needs to uniquely ground a specific object which will help a robot interact with that object.\
-                 Given this image, is the context enough of information to uniquely identify an object? Reply with a YES if that object can be distinguished uniquely with the given context. \
-                 If not, ask more questions to uniquely identify the object using its spatial location or other properties. If object is not present, ask more questions to get the right object. \
-                 Example - Context: The object that was placed was a bottle. Question: Which one was it? The rightmost, second from right, third from left, right and second from top, right and bottommost, or?"    
+def conversation(params, modified_prompt, last_frame, prompts, conversation_output):
+
+    """ Function to set up conversation between video understander and validator
+
+    Returns:
+        Object of interest concluded from conversation to be sent for grounding
+    """
+
+    # prompt_GPT = prompts['baseprompt_llm']  
+
+    for row in last_frame:
+        for i in range(len(row)):
+            row[i] = (row[i][2], row[i][1], row[i][0]) 
   
 
     #Changes required! 
-    prompt_LVLM = modified_prompt +  ". The video is from a robot's perspective. Return all answers from the robot's perspective. Do not ask questions"
+    prompt_LVLM = modified_prompt +  prompts['baseprompt_lvlm']
 
     flag = False
 
@@ -103,37 +109,63 @@ def conversation(params, modified_prompt, last_frame):
 
     while(not flag) :
 
-        # videoLLM_output = prompt_LVLM
 
-        socket = video_understanding.client_socket(params)
+        socket = video_understander.client_socket(params)
 
-        lvlm_output = video_understanding.send_query_to_server(prompt_LVLM, socket)
+        lvlm_output = video_understander.send_query_to_server(prompt_LVLM, socket)
+
+        
+        lvlm_output = eval(parser.to_cogVLM(lvlm_output, params, prompts))['output']
 
         if history == "":
-            prompt_GPT_main = "Context : " + lvlm_output + "Prompt:" + prompt_GPT
+            # prompt_GPT_main = "CONTEXT : " + lvlm_output + "Prompt:" + prompt_GPT
+            prompt_GPT_main = prompts['baseprompt_llm_1'] + lvlm_output + prompts['baseprompt_llm_2'] 
         
         else:
-            # prompt_GPT_main = "Consider " + history + "as your knowledge and memory." + "Context : " + lvlm_output + "Prompt:" + prompt_GPT
-            prompt_GPT_main = "Context : " + lvlm_output + "Prompt:" + prompt_GPT
+            prompt_GPT_main = "Consider " + history + "as your knowledge and memory." + prompts['baseprompt_llm_1'] + lvlm_output + prompts['baseprompt_llm_2'] 
+            # "Consider " + history + "as your knowledge and memory."
+            # prompt_GPT_main = "CONTEXT : " + lvlm_output + "Prompt:" + prompt_GPT
 
         print("prompt GPT main" , prompt_GPT_main)
         gpt_output = validator(params,prompt_GPT_main, last_frame)
-        prompt_LVLM = gpt_output + ". The video is from a robot's perspective. Return all answers from the robot's perspective. If options are given, just pick one from those and do not ask questions. "       
-        print(Color.GREEN + "Prompt LVLM" + prompt_LVLM + Color.RESET)
-        print(Color.RED + "LVLM output: "  + lvlm_output + Color.RESET)
-        print(Color.BLUE + "GPT output: "  + gpt_output + Color.RESET)
-        
+
+        print(Color.RED + "GPT Prompt: ", prompt_GPT_main)
+
+        # print(Color.RED + "LVLM output: "  + lvlm_output + Color.RESET)
+        # print(Color.BLUE + "GPT output: "  + gpt_output + Color.RESET)
+
+
         count = count + 1
+
+        with open(conversation_output, 'a') as file: 
+
+            init()
+
+            lvlm_output_color = "LVLM output: "  + lvlm_output
+            
+            gpt_output_color = "GPT output: "  + gpt_output 
+
+            file.write("Count: " + str(count) + "\n" + \
+                         lvlm_output_color + "\n" + \
+                        gpt_output_color + "\n\n" 
+                       )
+
         
-        if 'YES' in gpt_output or count >= 5:
+
+        
+        if 'YES' in gpt_output or count>=7:
             flag = True
             # prompt_LVLM = prompt_LVLM
 
             # output_list += [lvlm_output]
+        else:
+            gpt_output = parser.question_extractor(gpt_output, params, prompts)
+            prompt_LVLM = gpt_output + prompts['baseprompt_lvlm']     
+
         
-        # history += "Video LLM output :" + lvlm_output + "GPT output :" + gpt_output
-        history +=  lvlm_output 
+        history += "Video LLM output :" + lvlm_output + "GPT output :" + gpt_output
+        # history +=  lvlm_output 
 
 
-    return lvlm_output
+    return lvlm_output + gpt_output
     # return lvlm_output + " " + gpt_output
